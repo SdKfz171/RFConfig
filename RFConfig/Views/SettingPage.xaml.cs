3da454357019f56fb32cd13b7dbb821c0cef5732
Dongyoung_Kim <521ab@naver.com>
2018-01-03 18:17:45 +0900

    패리티 까지 구현

--- RFConfig/Views/SettingPage.xaml.cs
+++ RFConfig/Views/SettingPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,45 @@ namespace RFConfig.Views
     /// </summary>
     public partial class SettingPage : Page
     {
+        private SerialPort serial;
+
+        public string _PortName { get; set; }
+        public int _BaudRate { get; set; }
+        public int _DataBits { get; set; }
+        public Parity _Parity { get; set; }
+        public StopBits _StopBits { get; set; }
+
+
         public SettingPage()
         {
             InitializeComponent();
+
+            serial = new SerialPort();
+        }
+
+        private void SerialInit()
+        {
+            // string[] AvailablePorts = SerialPort.GetPortNames();
+            try
+            {
+                serial.PortName = _PortName;
+                serial.BaudRate = _BaudRate;
+                serial.DataBits = _DataBits;
+                serial.Parity = _Parity;
+                serial.StopBits = _StopBits;
+            }
+            catch
+            {
+
+            }
+            
+            serial.DataReceived += Serial_DataReceived;
+            
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
         }
 
         private void PortCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -32,17 +70,30 @@ namespace RFConfig.Views
 
         private void BaudRateCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            _BaudRate = int.Parse(((ComboBoxItem)BaudRateCombo.SelectedValue).Content.ToString());
         }
 
         private void DataBitsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            _DataBits = int.Parse(((ComboBoxItem)DataBitsCombo.SelectedValue).Content.ToString());
         }
 
         private void ParityCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            switch (ParityCombo.SelectedIndex)
+            {
+                case 0:
+                    _Parity = Parity.None;
+                    break;
 
+                case 1:
+                    _Parity = Parity.Odd;
+                    break;
+
+                case 2:
+                    _Parity = Parity.Even;
+                    break;
+            }
         }
 
         private void StopBitsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -51,6 +102,21 @@ namespace RFConfig.Views
         }
 
         private void FlowCtrlCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PortCombo_DropDownOpened(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Clicked");
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
